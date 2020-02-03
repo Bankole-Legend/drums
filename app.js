71e6fbb5f74ac6a5244a6852bed7bd5977e7136a
Pius Reis <pius.reis777@gmail.com>
2020-02-03 17:25:01 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * This is the drum kit project
+ * You are provided with an images folder which contains drum images. 
+ * You are required to attache each drum image to each alphabet in the html file (Watch the video to know which button has which image).
+ * 
+ * You are also provided with a sounds folder.
+ * 
+ * You are to attach each sound to the images and button in the html.
+ * 
+ * When you click on each button it should play the curresponding drum sound. 
+ * 
+ * Also When the curresponding key is pressed on the key board, the drum sound should be made. 
+ * 
+ * Key to each drum
+ * button w = tom-1.mp3
+ * button a = tom-2.mp3
+ * button s = tom-3.mp3
+ * button d = tom-4.mp3
+ * button j = kick-bass.mp3
+ * button k = snare.mp3
+ * button l = crash.mp3
+ * 
+ * NOTE
+ * Codes must be properly commented. 
+ * Use of ES6 Syntax is important. 
+ */
+
+
+
