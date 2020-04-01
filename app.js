2f28da9917d41f6fa6da33d19d1e02140192d552
Bankole_Adefemi <bankoledavidadefemi@gmail.com>
2020-04-01 02:52:47 +0100

    working drum kit

--- app.js
+++ app.js
@@ -24,6 +24,54 @@
  * Codes must be properly commented. 
  * Use of ES6 Syntax is important. 
  */
+ 
 
+// All the id for the audio files are given variable name same with the id name
+// the functions with attribute onclick are defined
+const w = document.querySelector("#w");
+function playAudio(){
+// The currentTime makes it posssible for the sound to play as many times as possible
+    w.currentTime = 0
+// The play(), this function comes with javascript as it is invoked it plays which ever audio
+    w.play();
+}
+const a = document.querySelector("#a");
+function playAudio1(){
+    a.currentTime = 0
+    a.play();
+}
+const s = document.querySelector("#s");
+function playAudio2(){
+    s.currentTime = 0
+    s.play();
+}
+const d = document.querySelector("#d");
+function playAudio3(){
+    d.currentTime = 0
+    d.play();
+}
+const j = document.querySelector("#j");
+function playAudio4(){
+    j.currentTime = 0
+    j.play();
+}
+const k = document.querySelector("#k");
+function playAudio5(){
+    k.currentTime = 0
+    k.play();
+}
+const l = document.querySelector("#l");
+function playAudio6(){
+    l.currentTime = 0
+    l.play();
+}
+//For the sound to play even when keyboard is pressed, an eevent listener is used
+//to listen a the keyCode which can be found online for eeach keyboard key and a
+//conditional statement is used to implement it. 
+document.addEventListener("keydown", (e) =>{ //also use function(e)
+    e.keyCode == 87?playAudio():e.keyCode == 65?playAudio1():
+    e.keyCode == 83?playAudio2():e.keyCode == 68?playAudio3():
+    e.keyCode == 74?playAudio4():e.keyCode == 75?playAudio5():playAudio6()
+})
 
 
